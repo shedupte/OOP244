fa3d5e14a7eb9df8b6cbf9112c2c8c8cb361590a
shedupte <33477021+shedupte@users.noreply.github.com>
2018-10-29 10:33:53 -0400

    week 6 inlab

---
+++ WS06/in-lab/Contact.h
@@ -0,0 +1,33 @@
+/*
+Name:Tenzin Shedup
+Student number:120664180
+Email:tshedup@myseneca.ca
+Workshop:Week_6
+Date:Sept 29,2018
+*/
+
+#ifndef SICT_CONTACT_H_
+#define SICT_CONTACT_H_
+
+namespace sict
+{
+	const int MAX_CHAR=20;
+	class Contact 
+	{
+		char m_Name[MAX_CHAR];
+		long long* m_PhoneNumbers;
+		int m_NoPhoneNumbers;
+
+		bool ValidCheck(const long long );
+	
+	public:
+		Contact();
+		Contact(const char*, const long long*, const int);
+		~Contact();
+		bool isEmpty() const;
+		void display() const;
+
+	
+	};
+}
+#endif
